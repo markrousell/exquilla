e3384ae668d309bb22c59e57df0849c09e22caf6
R. Kent James <kent@caspia.com>
2018-03-14 13:08:05 -0700

    Add an MPL license for the file ewsLogHelper.js

--- test/unit/ewsLogHelper.js
+++ test/unit/ewsLogHelper.js
@@ -1,4 +1,10 @@
-/*
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
+/* This file is adapted for use with ExQuilla from a Thunderbird source file */
+ 
+ /*
  * Makes everything awesome if you are Andrew.  Some day it will make everything
  *  awesome if you are not awesome too.
  *
